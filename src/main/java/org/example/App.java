d9f48ba967b6951af9f188e4862f9bf4eb80ca9c
BakChanGyu <qmrma987@gmail.com>
2023-03-06 15:10:01 +0900

    종료기능 구현, 올바르지 않은 명령어 처리

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,33 +6,35 @@ public class App {
     private Scanner sc;
     public App(Scanner sc) {
         this.sc = sc;
-
     }
-
     private int lastWiseSayingId = 1;
 
     public void run() {
         System.out.println("==명언 앱==");
-        System.out.print("명령) ");
 
-        String cmd = sc.nextLine().trim();
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
 
-        switch (cmd) {
-            case "종료" :
-                System.out.println("프로그램이 종료되었습니다.");
-                return;
-            case "등록":
-                System.out.println("명언: ");
-                String content = sc.nextLine().trim();
-                System.out.println("작가: ");
-                String authorName = sc.nextLine().trim();
+            if (cmd.isEmpty()) continue;
 
-                long id = ++lastWiseSayingId;
-                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-                break;
-            default:
-                System.out.printf("%s`(은)는 올바르지 않은 명령입니다.");
-                break;
+            switch (cmd) {
+                case "종료" :
+                    System.out.println("프로그램이 종료되었습니다.");
+                    return;
+                case "등록":
+                    System.out.println("명언: ");
+                    String content = sc.nextLine().trim();
+                    System.out.println("작가: ");
+                    String authorName = sc.nextLine().trim();
+
+                    long id = ++lastWiseSayingId;
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                default:
+                    System.out.printf("`%s`(은)는 올바르지 않은 명령입니다.", cmd);
+                    break;
+            }
         }
     }
 }
